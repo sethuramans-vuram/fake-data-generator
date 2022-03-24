17d13aad751b4470992a791ce8806dc34e9aeb6e
Sethuraman Srinivasan <sethu10121994@gmail.com>
2022-03-24 13:37:30 +0530

    :sparkles: added pdf functionality

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import { faker } from "@faker-js/faker";
 import { writeToStream } from '@fast-csv/format';
 import fs from 'fs';
 import path from 'path';
+import pdf from 'html-pdf';
 
-const CSV_ROW_COUNT = 10000;
+const CSV_ROW_COUNT = 1000;
 
 const createUser = () => {
   return {
@@ -19,7 +20,48 @@ const createUsers = (numUsers = 5) => {
     return Array.from({length: numUsers}, createUser);
 };
 
-writeToStream(fs.createWriteStream(path.resolve(__dirname, 'data.csv')), createUsers(CSV_ROW_COUNT), {
+const csvPath = path.resolve(__dirname, 'data.csv');
+const rows = createUsers(CSV_ROW_COUNT);
+writeToStream(fs.createWriteStream(csvPath), rows, {
     headers: Object.keys(createUser()),
     quoteColumns: false
+});
+
+let table_rows = rows.map((row) => {
+  return `<tr>
+    ${Object.values(row).map((col) => {
+      return `<td>${col}</td>`;
+    }).join("")}
+  </tr>`;
+});
+
+let html = `
+  <html>
+    <head>
+      <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+      </style>
+    </head>
+    <body>
+      <table>
+        <thead>
+          <tr>
+            ${Object.keys(createUser())
+              .map((col) => `<th>${col.charAt(0).toUpperCase() + col.slice(1)}</th>`).join("")}
+          </tr>
+        </thead>
+        <tbody>
+          ${table_rows.join("")}
+        </tbody>
+      </table>
+    </body>
+  </html>
+`;
+
+pdf.create(html, { format: "A4", orientation: "portrait" }).toFile("data.pdf", (err, res) => {
+  if (err) console.log(err);
+  console.log(res);
 });
